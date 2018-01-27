f70b078db95abd5e7118f63cc1a267d2facf6b06
Anatoly Kovalev <akovalev@allmax.team>
2018-01-27 10:59:23 +0300

    second commit

---
+++ popup/popup.js
@@ -0,0 +1,57 @@
+let maxLikes = 60;
+
+window.onload = () => {
+
+  const maxLikesInput = document.querySelector('#max-likes');
+  const start = document.querySelector('#start');
+  const pause = document.querySelector('#pause');
+  const stop = document.querySelector('#stop');
+
+  maxLikesInput.value = maxLikes;
+
+  maxLikesInput.addEventListener('input', () => {
+    maxLikes = maxLikesInput.value;
+  });
+
+  start.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          click: "start",
+          url: tabs[0].url,
+          maxLikes: maxLikesInput.value
+        },
+        () => {
+        console.log('like start');
+      });
+    });
+  });
+
+  pause.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, {click: "pause", url: tabs[0].url}, () => {
+        console.log('like pause');
+      });
+    });
+  });
+
+  stop.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, {click: "stop", url: tabs[0].url}, () => {
+        console.log('like stop');
+      });
+    });
+  });
+
+  console.log('chrome: ', chrome);
+};
+
+chrome.browserAction.onClicked.addListener(() => {
+  console.log('click icon');
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, {click: "icon", url: tabs[0].url}, () => {
+      console.log('click icon');
+    });
+  });
+});
